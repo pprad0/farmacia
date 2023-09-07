a8e426227a44f9afbe24acbd8ff56d9d4fbc0553
Pedro Melo <pedromarcosprado26@gmail.com>
2023-09-07 11:33:14 -0300

    Criacao da interface

---
+++ Farmacia/Repository/IProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Farmacia.Repository
+{
+    public interface IProdutoRepository
+    {
+        //CRUD
+        public void CriarProduto();
+        public void ListarProdutos();
+        public void ConsultarProdutoPorId(int id);
+        public void AtualizarProduto();
+        public void DeletarProduto(int id);
+    }
+}
